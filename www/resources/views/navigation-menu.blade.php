e70567aef127894b714d16ff30f63c8f0eb92624
Bruna Vitoria <brunavitoriati@gmail.com>
2023-01-11 09:55:41 -0300

    feat: altera as cores padrão do sistema e exibe a logo no navbar

--- www/resources/views/navigation-menu.blade.php
+++ www/resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <img src="/images/logoCP.png" class="block h-12 w-auto" alt="">
                     </a>
                 </div>
 
--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -14,6 +14,24 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'indigo-50': '#fff9ed',
+                'indigo-100': '#fff1d4',
+                'indigo-200': '#ffdfa9',
+                'indigo-300': '#ffbd59',
+                'indigo-400': '#fea439',
+                'indigo-500': '#fea439',
+                'indigo-600': '#ed6c09',
+                'indigo-700': '#c55109',
+                'indigo-800': '#9c4010',
+                'indigo-900': '#7e3610',
+            },
+            screens: {
+              sm: '640px',
+              md: '768px',
+              lg: '1024px',
+              xl: '1280px',
+            },
         },
     },
 
